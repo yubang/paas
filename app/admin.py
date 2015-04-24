1ce0cdab2b30b234b37eefeac454d7e103382e49
yubang <yubang@yubang-pc.(none)>
2015-04-24 19:17:41 +0800

    增加删除用户功能

--- app/admin.py
+++ app/admin.py
@@ -48,7 +48,7 @@ def account():
 @app.route("/userManager")        
 def userManager():
     "用户管理面板"
-    sql="select * from paas_account order by id desc"
+    sql="select * from paas_account where status != 3 order by id desc"
     dao=db.execute(sql)
     g.lists=map(objToDict,dao.fetchall())
     dao.close()
@@ -64,6 +64,8 @@ def appManager():
 @app.route("/userAdd",methods=['GET','POST'])  
 def userAdd():
     if request.method == "GET":
+        g.title=u"添加信息"
+        g.obj={}
         return render_template("admin/userAdd.html")
     else:
         username=request.form.get("username",None)
@@ -78,9 +80,57 @@ def userAdd():
         
         #把用户写入数据库
         args=map(sqlDeal,(username,password,status,realname,college,specialty))
-        print args
+        
         sql="insert into paas_account(username,password,status,realname,college,specialty) values('%s','%s','%s','%s','%s','%s')"%tuple(args)
         dao=db.execute(sql)
         dao.close()
         
-        return redirect("/admin/userManager")      
+        return redirect("/admin/userManager") 
+        
+        
+@app.route("/userMessage",methods=['GET','POST'])        
+def userMessage():
+    "修改用户信息"
+    
+    uid=request.args.get("id",None)
+    
+    if request.method == "GET":
+        g.title=u"修改信息"
+        
+        dao=db.execute("select * from paas_account where id = %s limit 1"%(sqlDeal(uid)))
+        g.obj=objToDict(dao.fetchone())
+        dao.close()
+        return render_template("admin/userAdd.html")
+    else:
+        
+        username=request.form.get("username",None)
+        password=request.form.get("password",None)
+        status=request.form.get("status",None)
+        realname=request.form.get("realname",None)
+        college=request.form.get("college",None)
+        specialty=request.form.get("specialty",None)
+        
+        if password == "":
+            args=map(sqlDeal,(username,status,realname,college,specialty,uid))
+            sql="update paas_account set username = '%s',status = '%s',realname = '%s',college = '%s',specialty='%s' where id = %s"%tuple(args)
+        else:
+            #加密密码
+            password=hashlib.md5(password).hexdigest()
+            args=map(sqlDeal,(username,password,status,realname,college,specialty,uid))
+            sql="update paas_account set username = '%s',password = '%s',status = '%s',realname = '%s',college = '%s',specialty='%s' where id = %s"%tuple(args)
+            
+        #修改用户信息
+        dao=db.execute(sql)
+        dao.close()
+        
+        return redirect("/admin/userManager")
+        
+@app.route("/deleteUser")
+def deleteUser():
+    "删除用户"
+    uid=request.args.get("id",None)
+    sql="update paas_account set status = 3 where id = "+sqlDeal(uid)
+    dao=db.execute(sql)
+    dao.close()
+    return redirect("/admin/userManager")
+             
